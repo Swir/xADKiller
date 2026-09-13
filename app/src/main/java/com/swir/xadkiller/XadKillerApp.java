c08dc5251a4729a4db59af28a12f37b1ee149a1d
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-13 09:07:48 +0200

    Add crash logger application

---
+++ app/src/main/java/com/swir/xadkiller/XadKillerApp.java
@@ -0,0 +1,18 @@
+package com.swir.xadkiller;
+
+import android.app.Application;
+import android.os.Build;
+
+public class XadKillerApp extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+        Thread.UncaughtExceptionHandler previous = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            try {
+                SystemLogStore.error(this, "CRASH", "Nieobsłużony wyjątek w wątku " + thread.getName(), throwable);
+            } catch (Throwable ignored) {}
+            if (previous != null) previous.uncaughtException(thread, throwable);
+        });
+        SystemLogStore.info(this, "APP", "Start procesu xADKiller 1.2.0 • Android " + Build.VERSION.RELEASE + " (API " + Build.VERSION.SDK_INT + ")");
+    }
+}
