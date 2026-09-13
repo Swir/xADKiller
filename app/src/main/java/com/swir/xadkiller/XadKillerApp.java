220d2701cb17d5fc05b59bea91784d84507042c2
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-13 10:34:59 +0200

    Update process diagnostics to v1.3.0

--- app/src/main/java/com/swir/xadkiller/XadKillerApp.java
+++ app/src/main/java/com/swir/xadkiller/XadKillerApp.java
@@ -13,6 +13,6 @@ public class XadKillerApp extends Application {
             } catch (Throwable ignored) {}
             if (previous != null) previous.uncaughtException(thread, throwable);
         });
-        SystemLogStore.info(this, "APP", "Start procesu xADKiller 1.2.1 • Android " + Build.VERSION.RELEASE + " (API " + Build.VERSION.SDK_INT + ")");
+        SystemLogStore.info(this, "APP", "Start procesu xADKiller 1.3.0 • Android " + Build.VERSION.RELEASE + " (API " + Build.VERSION.SDK_INT + ")");
     }
 }
