cdff9cd84441c94b010600d4eeb1cdcabeb1a18b
Wojciech <swirjestem@hotmail.com>
2026-09-13 20:11:15 +0200

    Load COMPAT engine with TITAN workers

--- browser-extension/common/service-worker.js
+++ browser-extension/common/service-worker.js
@@ -1,1 +1,1 @@
-importScripts("background.js", "live-signatures.js", "titan-engine.js", "static-boost.js");
+importScripts("background.js", "compat-engine.js", "live-signatures.js", "titan-engine.js", "static-boost.js");
