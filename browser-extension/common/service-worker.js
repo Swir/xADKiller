3cce60ff91fed5823ca0d362b04ebec8141bd02f
Wojciech <swirjestem@hotmail.com>
2026-09-14 16:23:45 +0200

    Wire temporary site Breakage Guard into service worker

--- browser-extension/common/service-worker.js
+++ browser-extension/common/service-worker.js
@@ -1,1 +1,1 @@
-importScripts("background.js", "live-signatures.js", "titan-engine.js", "static-boost.js");
+importScripts("background.js", "live-signatures.js", "titan-engine.js", "static-boost.js", "temporary-site-guard.js");
