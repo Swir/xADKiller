c15aaba4acbfd266a18df86738bb2fb38d2030bf
Wojciech <swirjestem@hotmail.com>
2026-09-17 18:08:03 +0200

    Load feed integrity and STANDARD core guards

--- browser-extension/common/service-worker.js
+++ browser-extension/common/service-worker.js
@@ -1,1 +1,1 @@
-importScripts("background.js", "live-signatures.js", "titan-engine.js", "static-boost.js", "temporary-site-guard.js");
+importScripts("feed-guard.js", "background.js", "live-signatures.js", "titan-engine.js", "static-boost.js", "standard-core-boost.js", "temporary-site-guard.js");
